4bc17eb8d3e6b89142b59392ac76a8e5db4ee19d
s2moon98 <s2moon98@naver.com>
2023-04-29 20:44:09 +0900

    제목 및 skill card 추가

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,64 +1,39 @@
 <template>
     <div class="profile-page">
         <section class="section-profile-cover section-shaped my-0">
-            <div class="shape shape-style-1 shape-primary shape-skew alpha-4">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+            <div class="shape shape-style-1 shape-primary shape-skew alpha-4"/>
         </section>
         <section class="section section-skew">
             <div class="container">
                 <card shadow class="card-profile mt--300" no-body>
                     <div class="px-4">
-                        <div class="row justify-content-center">
-                            <div class="col-lg-3 order-lg-2">
-                                <div class="card-profile-image">
-                                    <a href="#">
-                                        <img v-lazy="'img/theme/team-4-800x800.jpg'" class="rounded-circle">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 order-lg-3 text-lg-right align-self-lg-center">
-                                <div class="card-profile-actions py-4 mt-lg-0">
-                                    <base-button type="info" size="sm" class="mr-4">Connect</base-button>
-                                    <base-button type="default" size="sm" class="float-right">Message</base-button>
-                                </div>
-                            </div>
-                            <div class="col-lg-4 order-lg-1">
-                                <div class="card-profile-stats d-flex justify-content-center">
-                                    <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">10</span>
-                                        <span class="description">Photos</span>
-                                    </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
                         <div class="text-center mt-5">
-                            <h3>Jessica Jones
-                                <span class="font-weight-light">, 27</span>
-                            </h3>
-                            <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Bucharest, Romania</div>
-                            <div class="h6 mt-4"><i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer</div>
-                            <div><i class="ni education_hat mr-2"></i>University of Computer Science</div>
+                            <h2>문지은</h2>
+                            <div class="h6 font-weight-300"><i class="ni location_pin mr-2"></i>Moon Jieun</div>
+                            <div class="h5 mt-4"><i class="ni business_briefcase-24 mr-2"></i>백엔드 개발자 Backend Engineer</div>
                         </div>
-                        <div class="mt-5 py-5 border-top text-center">
+                        <div class="mt-5 py-5 border-top">
                             <div class="row justify-content-center">
                                 <div class="col-lg-9">
-                                    <p>An artist of considerable range, Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music, giving it a warm, intimate feel with a solid groove structure. An artist of considerable range.</p>
-                                    <a href="#">Show more</a>
+                                  <h5 class="text-primary text-uppercase font-weight-bold">Contact Information</h5>
+                                  <table>
+                                    <tr>
+                                      <th>Email</th>
+                                      <td>s2moon98@gmail.com</td>
+                                    </tr>
+                                    <tr>
+                                      <th>Phone</th>
+                                      <td>010-5542-2149</td>
+                                    </tr>
+                                    <tr>
+                                      <th>Blog</th>
+                                      <td><a href="https://velog.io/@s2moon98">https://velog.io/@s2moon98</a></td>
+                                    </tr>
+                                    <tr>
+                                      <th>Github</th>
+                                      <td><a href="https://github.com/s2moon98">https://github.com/s2moon98</a></td>
+                                    </tr>
+                                  </table>
                                 </div>
                             </div>
                         </div>
@@ -66,10 +41,64 @@
                 </card>
             </div>
         </section>
+        <section class="section section-lg pt-lg-0">
+          <div class="container">
+            <div class="row justify-content-center">
+              <div class="col-lg-12">
+                <div class="row row-grid">
+                  <div class="col-lg-4">
+                    <card class="border-0" hover shadow body-classes="py-5">
+                      <icon name="ni ni-check-bold" type="primary" rounded class="mb-4">
+                      </icon>
+                      <h5 class="text-primary text-uppercase">Backend Skills</h5>
+                      <ul class="navbar-nav ml-lg-auto">
+                        <li>Spring Boot, Spring Batch</li>
+                        <li>Java, Kotlin</li>
+                        <li>JPA, MyBatis</li>
+                        <li>Junit, Mockito</li>
+                        <li>Gradle, Maven</li>
+                        <li>Kafka</li>
+                      </ul>
+                    </card>
+                  </div>
+                  <div class="col-lg-4">
+                    <card class="border-0" hover shadow body-classes="py-5">
+                      <icon name="ni ni-istanbul" type="success" rounded class="mb-4">
+                      </icon>
+                      <h5 class="text-success text-uppercase">DevOps Skills</h5>
+                      <ul class="navbar-nav ml-lg-auto">
+                        <li>MySQL, MariaDB, H2</li>
+                        <li>AWS EC2, S3, RDS</li>
+                        <li>Docker</li>
+                        <li>Github Action</li>
+                      </ul>
+                    </card>
+                  </div>
+                  <div class="col-lg-4">
+                    <card class="border-0" hover shadow body-classes="py-5">
+                      <icon name="ni ni-planet" type="warning" rounded class="mb-4">
+                      </icon>
+                      <h5 class="text-warning text-uppercase">ETC</h5>
+                      <ul class="navbar-nav ml-lg-auto">
+                        <li>Linux</li>
+                        <li>Git / Github / Bitbucket</li>
+                        <li>Jira</li>
+                        <li>Confluence</li>
+                      </ul>
+                    </card>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
     </div>
 </template>
 <script>
 export default {};
 </script>
 <style>
+th, td {
+  padding: 5px;
+}
 </style>
